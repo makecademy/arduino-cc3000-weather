79e5058f7adc52f1b1ce0e4ac32934fce152cc8c
marcoschwartz <marcolivier.schwartz@gmail.com>
2015-08-26 12:20:44 +0200

    added status indicator

--- interface/script.js
+++ interface/script.js
@@ -5,9 +5,19 @@ $( document ).ready(function() {
 
     // Update temperature & humidity
     device.getVariable('temperature', function(data) {
+
+      // Update temperature display
       $('#temperature').html(data.temperature + ' C');
+
+      // Update status as well
+      if (data.connected = true) {
+      	$('#status').text('Online');
+      	$('#status').removeClass('red');
+      	$('#status').addClass('green');
+      }
     });
 
+    // Update humidity display
     device.getVariable('humidity', function(data) {
       $('#humidity').html(data.humidity + '%');
     });
